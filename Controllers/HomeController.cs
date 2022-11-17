57b4e3087bb7e0985ced2b3915ff59eb4cd6d527
NerijusKi <113437386+NerijusKi@users.noreply.github.com>
2022-11-17 11:02:18 +0200

    Be bin folderio

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using FirstMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(InputViewModel model)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+
+                return View("Index", model);
+
+            }
+            else
+            {
+                return View("Error");
+            }
+
+            //var manoObjektas = new ManoTest();
+            //manoObjektas.Query = kint;
+            //manoObjektas.Skaicius = skaicius;
+
+
+
+
+
+            //ViewBag.manoStringas = mystring; //ideda i view,vengti naudot
+            //ViewData["foo"] = mystring; //ideda i view,vengti naudot
+            //TempData["bar"] = mystring; //ideda i view,vengti naudot
+
+
+
+            //return View("Index", manoObjektas);
+            //return View("Index", model);
+        }
+
+        public IActionResult Verslas(string q, int sk)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/ManoTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVC.Models
+{
+    public class ManoTest
+    {
+
+        public int Skaicius { get; set; }
+        public string Query { get; set; }
+    }
+}
